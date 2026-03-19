d18679d9977755b9eb0348433fff62a3e18f3164
WondiradBezie <wondiradbeziemitikuhulu2@gmail.com>
2026-03-20 00:43:31 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
 
 from supabase import create_client
 import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
