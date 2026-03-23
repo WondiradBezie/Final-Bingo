569c06b52eef0c3479faa26a1d70f305e2f82207
WondiradBezie <wondiradbeziemitikuhulu2@gmail.com>
2026-03-23 11:12:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/database.db")
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
-WEBAPP_URL = os.getenv("WEBAPP_URL", "http://localhost:8000")
-PORT = int(os.getenv("PORT", 8000))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+REDIS_URL = os.getenv("REDIS_URL")
+WEBAPP_URL = os.getenv("WEBAPP_URL")
+PORT = int(os.getenv("PORT"))
 
 CARD_PRICE = 10
 PRIZE_PERCENT = 80
